650d0e54cb068705bba5d3d3c77188635c8a9a52
Forsaked <nico@piotraschke.net>
2017-06-29 18:06:31 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1457,8 +1457,12 @@ user_pref("privacy.firstparty.isolate.restrict_opener_access", true);
  ** spoof Navigator API (see section 2697) (FF56+)
    The version number will be rounded to the "nearest" multiple of 10
    [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1333651
- ** disable device sensor API (see 2512) (FF56+)		
+ ** disable device sensor API (see 2512) (FF56+)
    [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1369319
+ ** disable site specific zoom (see 2515) (FF56+)		
+   [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1369357		
+ ** disable gamepad API (see 2501) (FF56+)		
+   [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1337161
 ***/
 /* 2699a: enable privacy.resistFingerprinting (FF41+)
  * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=418986 ***/
